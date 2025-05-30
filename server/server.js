518d65683f7878544d8ccf9ba5ad3712575dc4d3
Deepika55555 <revatideepika@gmail.com>
2025-05-31 02:53:13 +0530

    deployment changes

--- server/server.js
+++ server/server.js
@@ -20,5 +20,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
+console.log("PORT environment variable is:", process.env.PORT);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
